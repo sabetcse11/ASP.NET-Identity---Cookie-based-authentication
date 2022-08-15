fdbb32c8f2503c7ec8cd771d5ee23f6e036d48b4
sabet <sabetcse11@gmail.com>
2022-08-15 12:37:18 +0600

    Add project files.

---
+++ Auth/MvcAppEmpty/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using MvcAppEmpty.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAppEmpty.Controllers
+{
+    [AllowAnonymous]
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Login model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            if (model.Email.Length > 0 && model.Password.Length > 0)
+            {
+                // Credentials are ok, now do the sign in
+                // Get the authentication Manager
+                // create a claim
+                // create claimsidentity
+                // sign in by manager & claimsidentity
+
+                IOwinContext context = Request.GetOwinContext();
+                IAuthenticationManager manager = context.Authentication;
+
+                var claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.Email, model.Email));
+                claims.Add(new Claim(ClaimTypes.Name, "Sabet"));
+
+                ClaimsIdentity identity = new ClaimsIdentity(claims: claims, authenticationType: "ApplicationCookie");
+                manager.SignIn(identities: identity);
+                return Redirect("/");
+            }
+            return View(model);
+        }
+
+        [Authorize]
+        public ActionResult LogOut()
+        {
+            IOwinContext context = Request.GetOwinContext();
+            IAuthenticationManager manager = context.Authentication;
+            manager.SignOut();
+            return Redirect("/");
+        }
+    }
+}
---
+++ Auth/MvcAppEmpty/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAppEmpty.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            string name = User.Identity.Name;
+            var browser = Request.Browser;
+            ViewBag.message = "Hello " + name + ". You have requested from " + browser.Browser;
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
